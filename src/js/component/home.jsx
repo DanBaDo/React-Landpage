115b6af18f313eb10a7da008bee05079c6187770
DanBaDo <daniel.banobre.dopico@protonmail.com>
2021-11-19 20:30:15 +0000

    Multiple Movie components test

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
-//include images into your bundle
+//Components
 import Title from "./Title.jsx";
+import Movie from "./Movie.jsx";
+
+//Resources
+import image from "../../img/rigo-baby.jpg";
+
+const movies = [
+	{
+		imgSrc: image,
+		alt: "Linda foto",
+		rating: "5.0",
+		title: "Corre que te las pelas",
+		year: "2043"
+	},
+	{
+		imgSrc: image,
+		alt: "Foto fea",
+		rating: "4.1",
+		title: "Cosas pequeñas",
+		year: "1999"
+	}
+];
+
+const movieComponets = movies.map((obj, idx) => (
+	<Movie
+		key={idx}
+		imgSrc={obj.imgSrc}
+		alt={obj.alt}
+		rating={obj.rating}
+		title={obj.title}
+		year={obj.year}></Movie>
+));
 
 //create your first component
 const Home = () => {
@@ -11,6 +42,9 @@ const Home = () => {
 				<h1>Component tests</h1>
 				<p>Title component:</p>
 				<Title title="Test"></Title>
+				<hr />
+				<p>Movie test</p>
+				{movieComponets}
 				<hr />
 			</div>
 		</div>
