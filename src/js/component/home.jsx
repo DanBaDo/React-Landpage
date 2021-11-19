c647c846a0ea7d6aecff99dfda1e98982589cb3a
DanBaDo <daniel.banobre.dopico@protonmail.com>
2021-11-19 12:29:04 +0000

    Component tests area in home.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,18 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-import TestDaniel from "./test-daniel.jsx";
+import Title from "./Title.jsx";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-			<TestDaniel> sdfsdf </TestDaniel>
+			<div className="tests">
+				<h1>Component tests</h1>
+				<p>Title component:</p>
+				<Title title="Test"></Title>
+				<hr />
+			</div>
 		</div>
 	);
 };
