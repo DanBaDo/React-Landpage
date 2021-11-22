a1dcfb1059f247aad4bd34e809f17ea9caa9e6c8
DanBaDo <daniel.banobre.dopico@protonmail.com>
2021-11-22 16:37:18 +0000

    Add random images.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,53 +10,53 @@ import image from "../../img/rigo-baby.jpg";
 
 const movies = [
 	{
-		imgSrc: image,
-		alt: "Linda foto",
+		imgSrc: "https://picsum.photos/200/300.webp",
+		alt: "Linda foto.",
 		rating: "5.0",
-		title: "Corre que te las pelas",
+		title: "Corre y no pares.",
 		year: "2043"
 	},
 	{
-		imgSrc: image,
-		alt: "Foto fea",
+		imgSrc: "https://picsum.photos/200/300.webp",
+		alt: "Foto fea.",
 		rating: "4.1",
-		title: "Cosas pequeñas",
+		title: "Cosas pequeñas.",
 		year: "1999"
 	},
 	{
-		imgSrc: image,
-		alt: "Big robot",
+		imgSrc: "https://picsum.photos/200/300.webp",
+		alt: "Big robot.",
 		rating: "2.3",
 		title: "Robocop XXXIV",
 		year: "2031"
 	},
 	{
-		imgSrc: image,
-		alt: "La llegada de los pollos del espacio",
+		imgSrc: "https://picsum.photos/200/300.webp",
+		alt: "Pollo espacial comiendo maíz.",
 		rating: "9.7",
-		title: "Cosas pequeñas",
+		title: "La llegada de los pollos del espacio.",
+		year: "2021"
+	},
+	{
+		imgSrc: "https://picsum.photos/200/300.webp",
+		alt: "Tunos comen berenjena en la playa.",
+		rating: "2.0",
+		title: "Recuerdos del año que viene.",
 		year: "1999"
 	},
 	{
-		imgSrc: image,
-		alt: "Foto fea",
-		rating: "4.1",
-		title: "Cosas pequeñas",
-		year: "1999"
-	},
-	{
-		imgSrc: image,
-		alt: "Foto fea",
-		rating: "4.1",
-		title: "Cosas pequeñas",
-		year: "1999"
+		imgSrc: "https://picsum.photos/200/300.webp",
+		alt: "Tipo colgando de canalón de desagüe.",
+		rating: "10.0",
+		title: "El loco de mi vecino.",
+		year: "2022"
 	}
 ];
 
 const movieComponets = movies.map((obj, idx) => (
 	<Movie
 		key={idx}
-		imgSrc={obj.imgSrc}
+		imgSrc={`${obj.imgSrc}?random=${idx}`}
 		alt={obj.alt}
 		rating={obj.rating}
 		title={obj.title}
